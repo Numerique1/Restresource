1640371e34f4a97d08998bd8589fd2529da24b3c
Nicolas Du <snicolas.du@gmail.com>
2018-10-05 14:21:36 +0200

    Add bundle file

---
+++ Numerique1RestresourcesBundle.php
@@ -0,0 +1,15 @@
+<?php
+namespace Numerique1\Components\RestresourcesBundle;
+
+use Doctrine\Bundle\DoctrineBundle\DependencyInjection\Compiler\DoctrineOrmMappingsPass;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class Numerique1RestresourcesBundle extends Bundle
+{
+    public function build(ContainerBuilder $container)
+    {
+        parent::build($container);
+
+    }
+}
