cc9af71a5c7911deea4e102de3f2412fa26c17a0
Nicolas Du <ShuyqcK@users.noreply.github.com>
2018-10-15 16:42:33 +0200

    Add guessExpr
    Add guessExpr function which reads $value from request parameter and search in for expr (like, eq, neg, etc...)

--- Repository/EntityMetadataFilterTrait.php
+++ Repository/EntityMetadataFilterTrait.php
@@ -1,5 +1,7 @@
 <?php
+
 namespace Numerique1\Components\Restresources\Repository;
+use Doctrine\ORM\QueryBuilder;
 
 /**
  * Trait EntityMetadataFilterTrait
@@ -16,20 +18,60 @@ trait EntityMetadataFilterTrait
      */
     public function filterByMetadataFieldNames(array $filters, Doctrine\ORM\QueryBuilder $qb = null)
     {
-        if(!$qb){
+        if (!$qb) {
             $qb = $this->createQueryBuilder('entity');
         }
-        foreach($filters as $name => $value){
+        foreach ($filters as $name => $value) {
             $value = strtolower($value);
             $fields = $this->getClassMetadata()->getFieldNames();
-            if(in_array($name, $fields)){
-                $qb->andWhere(
-                    $qb->expr()->like("LOWER({$qb->getRootAliases()[0]}.{$name})", ":value")
-                )
-                    ->setParameter(':value', "$value%");;
-                ;
+            if (in_array($name, $fields)) {
+                $this->createGuessExprFilter($qb,"LOWER({$qb->getRootAliases()[0]}.{$name})", $value);
             }
         }
         return $qb;
+    }
+
+    /**
+     * @param QueryBuilder $qb
+     * @param string $path
+     * @param string $value
+     */
+    public function createGuessExprFilter(QueryBuilder &$qb, string $path, string $value)
+    {
+        list($expr, $value) = $this->guessExpr($value);
+        $qb->andWhere(
+            $qb->expr()->$expr($path, ":value")
+        )
+        ->setParameter(':value', "$value");
+    }
+
+    /**
+     * Guess which expression to use from sent value ex : /api/users?name=likemarie
+     * @param $value
+     * @return array
+     */
+    public function guessExpr($value){
+        $operators = [
+            'like',
+            'lte',
+            'gte',
+            'neq',
+            'gt',
+            'lt',
+            'eq'
+        ];
+
+        $expr = 'eq';
+        foreach ($operators as $operator) {
+            $_operator = substr($value, 0, strlen($operator));
+            if ($_operator === $operator) {
+                $expr = $operator;
+
+                $_value = substr($value, strlen($operator), strlen($value));
+                $value = ($operator === 'like') ?  $_value."%" : $_value;
+                break;
+            }
+        }
+        return [$expr, $value];
     }
 }
