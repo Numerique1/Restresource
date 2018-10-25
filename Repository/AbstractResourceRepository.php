b3a98d17812470cb9e7d668e55e15cea55703814
Rousse Kevin <rousse_k@etna-alternance.net>
2018-10-25 16:20:52 +0200

    FIX - Sort, limit, offset

---
+++ Repository/AbstractResourceRepository.php
@@ -0,0 +1,54 @@
+<?php
+namespace Numerique1\Components\Restresources\Repository;
+
+use App\Octavio\Entity\Core\Internal;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Symfony\Bridge\Doctrine\RegistryInterface;
+
+/**
+ * Class AbstractResourceRepository
+ * @package Numerique1\Components\Restresources\Repository
+ */
+class AbstractResourceRepository extends ServiceEntityRepository implements ResourceRepositoryInterface
+{
+    /**
+     * @param QueryBuilder $qb
+     * @param              $sort
+     */
+    private function addOrderBy(&$qb, $sort)
+    {
+        foreach ($sort as $field => $type)
+        {
+            $qb->orderBy("{$qb->getRootAliases()[0]}.$field", $type);
+        }
+    }
+
+    /**
+     * @param array        $filters
+     * @param array|null   $sort
+     * @param integer|null $limit
+     * @param integer|null $offset
+     *
+     * @return array
+     */
+    public function cget(array $filters, array $sort = null, $limit = null, $offset = null)
+    {
+        $qb = $this->filterByMetadataFieldNames($filters);
+        $this->addOrderBy($qb, $sort);
+
+        return $qb->setMaxResults($limit)
+            ->setFirstResult($offset)
+            ->getQuery()
+            ->getResult();
+    }
+
+    /**
+     * @param $id
+     *
+     * @return null|object
+     */
+    public function get($id)
+    {
+        return $this->find($id);
+    }
+}
